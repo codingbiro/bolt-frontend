7199c6411800e5ac042947290e2408983ecde756
codingbiro <hello@birovince.com>
2020-09-16 22:50:46 +0200

    Formik added

---
+++ src/components/FormikTextField.tsx
@@ -0,0 +1,161 @@
+import TextField, { TextFieldProps } from "@material-ui/core/TextField";
+import React, { useCallback } from "react";
+import { fade, FormControlLabel, makeStyles } from "@material-ui/core";
+import { useFormikContext } from "formik";
+import cn from "classnames";
+
+function objectPropertyByString<T = any>(
+  object: Record<string, unknown>,
+  propertyString: string
+): T | undefined {
+  try {
+    return propertyString
+      .split(".")
+      .reduce(
+        (objectLevel, index) => objectLevel[index] as Record<string, unknown>,
+        object
+      ) as T;
+  } catch {
+    return undefined;
+  }
+}
+
+const useStyles = makeStyles(() => ({
+  red: {
+    color: "red",
+  },
+  form: {
+    width: "100%",
+    marginTop: "24px",
+  },
+  submit: {
+    marginTop: "24px",
+  },
+  label: {
+    width: "100%",
+    alignItems: "flex-start",
+    margin: 0,
+    "& .MuiFormControlLabel-label": {
+      paddingBottom: "8px",
+      fontSize: "12px",
+      fontWeight: "600",
+      "&.Mui-disabled": {
+        color: fade("#000", 0.4),
+      },
+    },
+    "&.no-label  .MuiFormControlLabel-label": {
+      padding: 0,
+    },
+  },
+  field: {
+    height: "24px",
+    width: "100%",
+    minWidth: "40px",
+    borderRadius: "8px",
+    "& .MuiInputBase-root, &.MuiInputBase-root": {
+      height: "24px",
+      "& input": {
+        paddingTop: 0,
+        paddingBottom: 0,
+        height: "24px",
+        borderRadius: "8px",
+        border: 0,
+        fontSize: "14",
+
+        "&.Mui-disabled": {
+          background: fade("#9ea18f", 0.1),
+        },
+      },
+      "& .MuiOutlinedInput-notchedOutline": {
+        height: "24px",
+        top: 0,
+        "& legend": {
+          display: "none",
+        },
+      },
+      "&:not(.Mui-focused):not(.Mui-error)": {
+        "& .MuiOutlinedInput-notchedOutline": {
+          borderColor: "#9ea18f",
+        },
+        "&:hover .MuiOutlinedInput-notchedOutline": {
+          borderColor: "#6c6e65",
+        },
+      },
+    },
+  },
+  error: {
+    paddingTop: "8px",
+    color: "red",
+    fontWeight: "bold",
+  },
+}));
+
+type Props = Omit<TextFieldProps, "name"> & {
+  name: string;
+  value?: string;
+  onChange?: (value: string) => void;
+};
+
+const FormikTextField: React.FC<Props> = ({
+  label: _label,
+  name,
+  onChange,
+  required,
+  className,
+  ...propsToPass
+}) => {
+  const classes = useStyles();
+  const formikContext = useFormikContext<Record<string, unknown>>();
+  if (formikContext === undefined)
+    throw new Error(
+      "Cannot get Formik context. Have you use the AppTextField as child of a <Formik> component?"
+    );
+
+  const { values, setFieldValue, touched, errors } = formikContext;
+
+  let value = objectPropertyByString(values, name);
+  if (value === null || value === undefined) value = "";
+
+  const label = useCallback(() => {
+    return _label && required ? `${_label}*` : _label;
+  }, [_label, required]);
+
+  return (
+    <div>
+      <FormControlLabel
+        labelPlacement="top"
+        className={cn(classes.label, { "no-label": !label() })}
+        label={label()}
+        control={
+          <TextField
+            {...propsToPass}
+            name={name}
+            value={value}
+            required={required}
+            variant="outlined"
+            error={
+              objectPropertyByString(touched, name) &&
+              !!objectPropertyByString(errors, name)
+            }
+            onChange={(event) => {
+              const newValue = event.target.value;
+              setFieldValue(name, newValue);
+              if (onChange) {
+                onChange(newValue);
+              }
+            }}
+            className={cn(className, classes.field)}
+          />
+        }
+      />
+
+      <div className={classes.error}>
+        {objectPropertyByString(touched, name)
+          ? objectPropertyByString(errors, name)
+          : undefined}
+      </div>
+    </div>
+  );
+};
+
+export default FormikTextField;
--- src/containers/Auth/Auth.tsx
+++ src/containers/Auth/Auth.tsx
@@ -1,17 +1,142 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  FormControl,
+  makeStyles,
+} from "@material-ui/core";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikTextField from "../../components/FormikTextField";
 // import useHideNavigation from "../../components/useHideNavigation";
 
 const useStyles = makeStyles(() => ({
   red: {
     color: "red",
   },
+  form: {
+    width: "100%",
+    marginTop: "24px",
+  },
+  submit: {
+    marginTop: "24px",
+  },
+  authContainer: {
+    maxWidth: "600px",
+    width: "50%",
+    margin: "auto",
+  },
+  whiteColor: {
+    color: "#fff !important",
+  },
 }));
+
+interface Form {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const validation = Yup.object().shape<Form>({
+  firstName: Yup.string().required("Please fill out this field."),
+  lastName: Yup.string().required("Please fill out this field."),
+  email: Yup.string().required("Please fill out this field."),
+  password: Yup.string().required("Please fill out this field."),
+});
 
 const Default: React.FC = () => {
   const classes = useStyles();
   // useHideNavigation();
-  return <div className={classes.red}>Hey Auth</div>;
+  return (
+    <div className={classes.authContainer}>
+      <h1 className={classes.red}>Signup</h1>
+      <Formik<Form>
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+        }}
+        validationSchema={validation}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        {({ handleSubmit, isSubmitting }) => (
+          <Form
+            noValidate
+            onSubmit={(e) => handleSubmit(e)}
+            className={classes.form}
+          >
+            <FormControl margin="normal" required fullWidth>
+              <FormikTextField
+                id="firstName"
+                name="firstName"
+                type="text"
+                placeholder="First Name"
+                autoFocus
+                required
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <FormikTextField
+                id="lastName"
+                name="lastName"
+                type="text"
+                placeholder="Last Name"
+                required
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <FormikTextField
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+                required
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <FormikTextField
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                autoComplete="password"
+                required
+              />
+            </FormControl>
+            <Box display="flex" justifyContent="center" py={3}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign up
+                {isSubmitting && (
+                  <Box display="flex" alignItems="center" pr={1}>
+                    <CircularProgress
+                      size={16}
+                      thickness={5}
+                      color="inherit"
+                      className={classes.whiteColor}
+                    />
+                  </Box>
+                )}
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default Default;
